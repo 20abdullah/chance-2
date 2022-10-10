47d3cafe2dab19a8526c185b5cbbb03eb89539a8
20abdullah <113922299+20abdullah@users.noreply.github.com>
2022-10-10 22:58:41 +0200

    Add files via upload

---
+++ contion.php
@@ -0,0 +1,8 @@
+<?php 
+$server ="sql8.freesqldatabase.com";
+$username ="sql8525610";
+$password ="NFQQtP6H1U";
+$dbname ="sql8525610";
+
+$conn =mysqli_connect($server,$username,$password,$dbname);
+?>
